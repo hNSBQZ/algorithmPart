7196c418b7e7a96ce03d7f5acdb349a7dc174c11
hqz <2462546443@qq.com>
2022-10-05 21:46:25 +0800

    10/5 21：44

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import myLearn.*;
+import myLearn.utils.CsvAndXls;
 import myLearn.utils.FiveTuple;
 import myLearn.utils.FourTuple;
 import org.ejml.simple.SimpleMatrix;
@@ -16,6 +17,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        //Csv与excel相互转换的例子
+        CsvAndXls.CsvToXlsx("JDT.csv","JDT.xls");
+        CsvAndXls.XlsxToCsv("JDT.xls","abc.csv");
+        //knn训练例子
 
         FourTuple<SimpleMatrix, Double, Double, Double> dataPack=UsingController.trainingKnnControl(9,"JDT.csv","Knn2",0.8);
         System.out.println("混淆矩阵，准确率，精度，召回率");
@@ -23,6 +28,8 @@ public class App
         System.out.println(dataPack.second);
         System.out.println(dataPack.third);
         System.out.println(dataPack.fourth);
+
+        //线性回归训练例子
         FiveTuple<double[],SimpleMatrix, Double, Double, Double> dataPack1=UsingController.trainingLogisticRegressionControl(3000,0.000001,0,0.1,797,"JDT.csv","lgr2",0.8);
         System.out.println("混淆矩阵，准确率，精度，召回率");
         //first是一个数组，里面为损失函数变化值
@@ -30,6 +37,9 @@ public class App
         System.out.println(dataPack1.third);
         System.out.println(dataPack1.fourth);
         System.out.println(dataPack1.fifth);
+
+        //用户预测例子
         UsingController.predictControl("LogisticRegression1","Lucene.csv");
+
     }
 }
